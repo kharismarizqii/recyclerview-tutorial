f014db968866e3b035ae395db050f9c8c7645ded
kharismarizqii <rizqikharisma@gmail.com>
2020-04-24 17:30:07 +0700

    rv 3

--- app/src/main/java/com/kharismarizqii/recyclerviewexample/MainActivity.kt
+++ app/src/main/java/com/kharismarizqii/recyclerviewexample/MainActivity.kt
@@ -2,11 +2,36 @@ package com.kharismarizqii.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val list = ArrayList<SuperHero>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rv_superhero.setHasFixedSize(true)
+        list.addAll(getListSuperHero())
+        rv_superhero.layoutManager = LinearLayoutManager(this)
+        val superHeroAdapter = SuperHeroAdapter(list)
+        rv_superhero.adapter = superHeroAdapter
+    }
+
+    private fun getListSuperHero(): ArrayList<SuperHero>{
+        val name = resources.getStringArray(R.array.data_name)
+        val desc = resources.getStringArray(R.array.data_desc)
+
+        val listSuperHero = ArrayList<SuperHero>()
+        for(i in name.indices){
+            val superHero = SuperHero(
+                name[i],
+                desc[i]
+            )
+            listSuperHero.add(superHero)
+        }
+        return listSuperHero
     }
 }
